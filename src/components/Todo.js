b99c2597be76a6b7da5ccb93a7c80fbcfc8d3835
akkii03 <akkiindoria72@gmail.com>
2022-08-02 16:19:32 -1200

    remove extra space

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,12 +2,12 @@ import React from 'react';
 const Todo = ({dispatch,state}) => {
     return (
         <div className='todo'>
-            <div className='todo- title'>  
+            <div className='todo-title'>  
             {
                 <ul>{
                 state.map((item)=>{
                     return(
-                        <li id={item.id} key={item.id}>{item.taskValue}<button onClick={()=>dispatch({type:"DELETE",payload:item.id})} className="todo- delete">DELETE</button></li>
+                        <li id={item.id} key={item.id}>{item.taskValue}<button onClick={()=>dispatch({type:"DELETE",payload:item.id})} className="todo-delete">DELETE</button></li>
                     )
                 })
                      }
